35851de0fefa9e66a3e4af1ce5fdafb3aa528b7b
NatchanonCS <kongsnpz@gmail.com>
2025-01-15 10:19:51 +0700

    git practice 15/1/25

---
+++ LabSheet06/src/TestStudent.java
@@ -0,0 +1,20 @@
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+		
+		Student student1 = new Student ();
+		Student student2 = new Student ();
+		
+		student1.setStudentDetails("Sriyakorn", new double[] {85.0,78.0,92.0});
+		student2.setStudentDetails("Sumsoo", new double[] {68.0,66.5,});
+		
+		System.out.println("Student 1 details:");
+		student1.displayStudentDetails();
+		
+		System.out.println("Student 2 details:");
+		student2.displayStudentDetails();
+
+	}
+
+}
